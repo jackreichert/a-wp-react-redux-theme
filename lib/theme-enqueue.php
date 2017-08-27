5328ddc9c8c5bfa05360c50fa91c3798b7fdc371
Jack Reichert <awesome@jackreichert.com>
2017-08-27 17:48:48 -0400

    update bootstrap

--- lib/theme-enqueue.php
+++ lib/theme-enqueue.php
@@ -11,16 +11,12 @@ if ( ! class_exists( 'Theme_Enqueue' ) ) :
 		}
 
 		function init() {
-			add_action( 'wp_enqueue_scripts', [ $this, 'bootstrap' ], 5 );
 			add_action( 'wp_enqueue_scripts', [ $this, 'theme' ], 20 );
 		}
 
-		function bootstrap() {
-			wp_enqueue_style( 'bootstrap4-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css', [], '4a6' );
-		}
-
 		function theme() {
-			wp_enqueue_script( 'ReactTheme-js', get_template_directory_uri() . '/bundle.js', [ 'jquery' ], date( 'YmdHis' ), true );
+			wp_enqueue_style( 'bootstrap4-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', [], '4b' );
+			wp_enqueue_script( 'ReactTheme-js', get_template_directory_uri() . '/bundle.js', [ 'jquery' ], '08272017', true );
 			wp_localize_script( 'ReactTheme-js', 'RT_API', array(
 				'root'            => esc_url_raw( rest_url() ),
 				'nonce'           => wp_create_nonce( 'wp_rest' ),
@@ -29,7 +25,7 @@ if ( ! class_exists( 'Theme_Enqueue' ) ) :
 				'categories'      => $this->get_categories_with_links(),
 				'current_user'    => wp_get_current_user()
 			) );
-			wp_enqueue_style( get_stylesheet(), get_template_directory_uri() . '/bundle.css' );
+			wp_enqueue_style( 'theme_stylesheet', get_template_directory_uri() . '/bundle.css', [ 'bootstrap4-css' ], '08272017' );
 		}
 
 		function get_categories_with_links() {
@@ -37,6 +33,7 @@ if ( ! class_exists( 'Theme_Enqueue' ) ) :
 			foreach ( $categories as $i => $category ) {
 				$categories[ $i ]->link = get_category_link( $category->term_id );
 			}
+
 			return $categories;
 		}
 	}
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import Search from './search';
 class Header extends Component {
     render() {
         return (
-            <header className="navbar navbar-toggleable-sm navbar-light bg-faded">
+            <header className="navbar navbar-expand-lg navbar-light bg-light">
                 <h1 className="navbar-brand"><Link to='/'>{RT_API.siteName}</Link></h1>
                 <nav className="collapse navbar-collapse">
                     <Menu name="main_menu" />
