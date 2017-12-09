fc2eb24659420b3007800e9daed210c4b3e9337d
Jack Reichert <awesome@jackreichert.com>
2017-12-09 18:20:54 -0500

    moved footer out of react for wpadminbar

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
                     &nbsp;|&nbsp;Proudly Powered by&nbsp;
                     <a href="https://wordpress.org/">WordPress</a>&nbsp;&amp;&nbsp;<a href="https://github.com/jackreichert/a-wp-react-redux-theme">A React+Redux WordPress theme</a>
                 </div>
-                <?php wp_footer(); ?>
             </footer>
 
         </section>
     </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
