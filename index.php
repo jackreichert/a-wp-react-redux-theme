cc886f3f91415b4e8a42b8721a9519770950b9c4
Jack Reichert <awesome@jackreichert.com>
2017-12-09 15:56:18 -0500

    made site functional without javascript

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php get_header(); ?>
-
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article class="<?php post_class(); ?>">
-            <?php if (has_post_thumbnail()): ?>
-                <?php the_post_thumbnail(); ?>
-            <?php endif; ?>
-            <div class="card-block">
-                    <?php the_title(); ?>
-            </div>
-        </article>
-	<?php endwhile; else : ?>
-        There doesn't seem to be anything here...
-	<?php endif; ?>
-
+<div>
+    <main id="postsContainer" class="<?php Theme_Helpers::get_class( 'card-columns', '' ); ?>">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+                <article <?php post_class( Theme_Helpers::get_class( 'card archive', 'card single w-75', false ) ); ?>>
+					<?php if ( has_post_thumbnail() ): ?>
+						<?php the_post_thumbnail( 'medium', array( 'class' => 'card-img-top img-fluid' ) ); ?>
+					<?php endif; ?>
+                    <div class="card-block">
+                        <header class="card-title"><a href="<?php the_permalink(); ?>">
+                                <h1><?php the_title(); ?></h1></a></header>
+                        <div class="meta">
+                            <div class="cats"><?php echo $categories_list = get_the_category_list( __( ', ', 'react-theme' ) ); ?></div>
+                        </div>
+                        <div class="card-text"><p><?php the_content(); ?></p>
+                        </div>
+                    </div>
+                    <footer></footer>
+                </article>
+			<?php endwhile; ?>
+		<?php else : ?>
+            There doesn't seem to be anything here...
+		<?php endif; ?>
+    </main>
+    <div class="navigation"><p><?php posts_nav_link(); ?></p></div>
+</div>
 <?php get_footer(); ?>
